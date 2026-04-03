56ebc3b230e9cab60a9e687bed2b850ce68cb25b
surajmeche <surajmechesuraj@gmail.com>
2026-04-03 13:22:33 +0545

    Initial PWA project commit

---
+++ src/script.js
@@ -0,0 +1,11 @@
+if("serviceWorker" in navigator){
+navigator.serviceWorker.register("sw.js").then(registration =>{
+    console.log("SW Registered !");
+    console.log("Registration");
+}).catch(error =>{
+    console.log("SW Registration Failed");
+    console.log("SW Registration")
+})
+}else{
+
+}
